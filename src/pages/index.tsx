5cef49419ebf7807369b79c61d08b4bc5b82c38f
João Gabriel <jgomachado@hotmail.com>
2023-05-24 10:13:03 -0300

    styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,30 @@
-import { styled } from '../styles'
+import Image from 'next/image'
+import { HomeContainer, Product } from '../styles/pages/home'
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-})
+import camisa1 from '../assets/camisetas/1.png'
+import camisa2 from '../assets/camisetas/2.png'
+import camisa3 from '../assets/camisetas/3.png'
 
 export default function Home() {
-  return <Button>enviar</Button>
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={camisa1} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camisa2} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  )
 }
