4caddb245bb43abb7c376116ded06707e19177c1
João Gabriel <jgomachado@hotmail.com>
2023-05-24 17:10:06 -0300

    alt

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
 import Image from 'next/image'
+import { useKeenSlider } from 'keen-slider/react'
+
 import { HomeContainer, Product } from '../styles/pages/home'
 
 import camisa1 from '../assets/camisetas/1.png'
 import camisa2 from '../assets/camisetas/2.png'
 import camisa3 from '../assets/camisetas/3.png'
 
+import 'keen-slider/keen-slider.min.css'
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camisa1} alt="" width={520} height={480} />
 
         <footer>
@@ -17,8 +28,26 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camisa2} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camisa3} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camisa3} alt="" width={520} height={480} />
 
         <footer>
           <strong>Camiseta X</strong>
