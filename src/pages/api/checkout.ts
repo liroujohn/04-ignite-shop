708acbf869ed6527a4759190eb78805618357af5
João Gabriel <jgomachado@hotmail.com>
2023-06-01 11:32:37 -0300

    alt

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -1,5 +1,6 @@
 import { stripe } from '@/src/lib/stripe'
-import { NextApiRequest, NextApiResponse } from 'next'
+import { GetServerSideProps, NextApiRequest, NextApiResponse } from 'next'
+import Stripe from 'stripe'
 
 export default async function handler(
   req: NextApiRequest,
@@ -15,7 +16,7 @@ export default async function handler(
     return res.status(400).json({ error: 'Price ID is required' })
   }
 
-  const successUrl = `${process.env.NEXT_URL}/success`
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
   const cancelUrl = `${process.env.NEXT_URL}/`
 
   const checkoutSession = await stripe.checkout.sessions.create({
