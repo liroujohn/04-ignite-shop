1262f500f3b3283c6d84c18b63af44c99bf6dadd
João Gabriel <jgomachado@hotmail.com>
2023-06-06 15:15:33 -0300

    alt

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import logoImg from '../../assets/logo.svg'
+import Link from 'next/link'
+import { HeaderContainer } from './styles'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <Link href="/">
+        <Image src={logoImg} alt="" />
+      </Link>
+    </HeaderContainer>
+  )
+}
